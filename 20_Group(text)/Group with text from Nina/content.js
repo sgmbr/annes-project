c0c34561d0451414fdbb555d1d1e5f4ff85cabc2
sgmbr <satoshi.sgmr@gmail.com>
2017-03-27 16:18:44 +1300

    Organized codes
    Deleted unused codes and consol.log's.
    Added comments to understand.
    Moved styles set in JavaScript codes to css file.

--- 20_Group(text)/Group with text from Nina/content.js
+++ 20_Group(text)/Group with text from Nina/content.js
@@ -1,4 +1,4 @@
-/* jshint undef: true, unused: true, esversion: 6, asi: true */
+/* jshint undef: true, unused: true, esversion: 6, asi: true, browser: true, jquery: true */
 
 var description = [];
 var answers = [];
@@ -7,73 +7,77 @@ var ano = 0;
 var incorrect = 0;
 $(document).ready(function() {
     // load xml file using jquery ajax
+    // TODO: change to native JavaScript
     $.ajax({
         type: "GET",
         url: "store.xml",
         dataType: "xml",
         success: function(d) {
             $(d).find('block').each(function() {
-                var $book = $(this);
+                var $book = $(this); // = block
+                // create boxes under #box
                 var q = 0;
                 $book.find('box').each(function() {
-                    $("#box").append('<div id=q' + q + '><p style="text-align:center;background:#ffd;">' + $(this).text() + '</p></div>');
+                    $("#box").append('<div id=q' + q + '><p>' + $(this).text() + '</p></div>');
                     description.push($(this).text());
-                    var iid = $(this).text();
                     q++;
                 });
-                console.log(q);
+                // create answer cards under #ans
                 var a = 0;
                 $book.find('question').each(function() {
-                    var ele = $('<div id=a' + a + '><p style="text-align:center;margin-left:8px;margin-right:8px;">' + $(this).text() + '</p></div>');
-                    // console.log($(this).text());
+                    var ele = $('<div id=a' + a + '><p>' + $(this).text() + '</p></div>');
                     $("#ans").append(ele);
-                    var id = $(this).text();
                     answers.push($(this).text());
-                    console.log(answers);
                     a++;
                 });
-                console.log(description.length);
             });
 
-
+            // shorthand for $(document).ready(function() { ... });
             $(function() {
                 var parent = $("#ans");
                 var divs = parent.children();
+                // divs.length is implicit true if not 0
                 while (divs.length) {
+                    // randomizing the order of answer cards
                     parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
                 }
+                // counting just for debugging purpose?
                 qno = $("#box > div").length;
                 ano = $("#ans > div").length;
-                console.log(qno);
-                console.log(ano);
             });
         }
     });
 
+    // shorthand for ajax function.
+    //   url: 'store.xml',
+    //   success: function(d) {...}
     $.get('store.xml', function(d) {
         var n = 0;
         var aa = 0;
+        // second parameter(d) is context
         $('block', d).children().each(function() {
-            var x = $(this).find('box').text();
+            // setting the boxes droppable by using jQuery UI
             $('#q' + n).droppable({
                 accept: '#ans div',
                 hoverClass: 'hovered',
                 drop: handleCardDrop
             });
+
+            // question element is not used inside.
+            // just used to loop for the number of answers.
             $(this).find('question').each(function() {
-                var y = $(this).text();
-                console.log($(this).text());
+                // setting attribute (e="q0" or e="q1") to each answer cards
                 $('#a' + aa).attr('e', 'q' + n);
+
+                // setting the answer cards draggable by using jQuery UI
                 $('#a' + aa).draggable({
                     containment: 'body',
                     stack: '#ans div',
                     cursor: 'move',
                     revert: true
                 });
-                //console.log(aa);
                 aa++;
             });
-            // alert($(this).text());
             n++;
         });
     });
@@ -87,11 +91,8 @@ function handleCardDrop(event, ui) {
 
     if (slotNumber == cardNumber) {
         correct++;
-        console.log(correct);
         ui.draggable.addClass('correct');
         ui.draggable.draggable('disable');
-        //$(this).droppable( 'disable' );
-        //ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
         ui.draggable.draggable('option', 'revert', false);
     } else {
         incorrect++;
@@ -101,27 +102,13 @@ function handleCardDrop(event, ui) {
 function coun() {
     var url = $(location).attr('href');
     var tech = url.substring(url.indexOf('=') + 1);
-    console.log(tech);
-    console.log('inc' + incorrect);
-    console.log('an' + ano);
-    console.log('ques' + qno);
-    var cid = $('#4').val();
-    console.log('break');
-    console.log('the course id' + cid);
     var unplaced = ano - correct;
     var aqminus = ano * (qno - 1);
-    console.log('unp' + unplaced);
     var score = 100 - (200 * incorrect / aqminus) - (100 * unplaced / ano);
-    var sc = $('#region-main', window.parent.document).find('input[name="mark"]').attr('value', score);;
-    console.log('scoe' + sc);
-    console.log(score);
     $.ajax({
         url: 'score.php', //This is the current doc
         type: "POST",
         data: 'event=' + tech + '&score=' + score,
-        success: function(data) {
-            console.log(data);
-        }
     })
 
     $('#res').show();
@@ -139,31 +126,4 @@ function coun() {
 function hide() {
     $('#res').hide();
     location.reload();
-}
-
-function saveScore() {
-    var url = $(location).attr('href');
-    var tech = url.substring(url.indexOf('=') + 1);
-    console.log(tech);
-    console.log('inc' + incorrect);
-    console.log('an' + ano);
-    console.log('ques' + qno);
-    var cid = $('#4').val();
-    console.log('break');
-    console.log('the course id' + cid);
-    var unplaced = ano - correct;
-    var aqminus = ano * (qno - 1);
-    console.log('unp' + unplaced);
-    var score = 100 - (200 * incorrect / aqminus) - (100 * unplaced / ano);
-    var sc = $('#region-main', window.parent.document).find('input[name="mark"]').attr('value', score);;
-    console.log('scoe' + sc);
-    console.log(score);
-    $.ajax({
-        url: 'score.php', //This is the current doc
-        type: "POST",
-        data: 'event=' + tech + '&score=' + score,
-        success: function(data) {
-            console.log(data);
-        }
-    });
 }
--- 20_Group(text)/Group with text from Nina/wrapper.js
+++ 20_Group(text)/Group with text from Nina/wrapper.js
@@ -1,18 +1,5 @@
 /* jshint undef: true, unused: true, esversion: 6, asi: true */
 
-/*
-function NavBtnOver(Btn){
-  if (Btn.className != 'NavButtonDown'){Btn.className = 'NavButtonUp';}
-}
-
-function NavBtnOut(Btn){
-  Btn.className = 'NavButton';
-}
-
-function NavBtnDown(Btn){
-  Btn.className = 'NavButtonDown';
-}
-*/
 //this script is needed to set the dimensions of the iframe
 function resizeIframe(obj) {
     obj.style.height = (obj.contentWindow.document.body.scrollHeight) + 'px'
