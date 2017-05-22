fb87a8be84296014e0a79ce576e3996cf7026202
sgmbr <satoshi.sgmr@gmail.com>
2017-05-22 16:22:04 +1200

    Delete old Group program

--- 20_Group/program/src/controller.js
+++ 20_Group/program/src/controller.js
@@ -1,25 +1,42 @@
 /* jshint undef: true, unused: true, esversion: 6, asi: true, browser: true, jquery: true */
 
 class Controller {
-    constructor(quiz, View) {
-        Controller.theQuiz = quiz
-        Controller.view = View
-        Controller.setUp()
+    constructor(newQuiz, newView) {
+        Controller.myQuiz = newQuiz     // Model: Quiz instance
+        Controller.myView = newView     // View: View class
+
+        Controller.createQuizElements()
+        Controller.setUpEventListeners()
     }
 
-    static setUp() {
-        window.addEventListener('submitEvent', Controller.submitEventHandler, false)
-        window.addEventListener('tryAgainEvent', Controller.tryAgainEventHandler, false)
-        window.addEventListener('scoreUpdateEvent', Controller.scoreUpdateEventHandler, false)
-        window.addEventListener('resizeIframeEvent', Controller.resizeIframeEventHandler, false)
+    static createQuizElements() {
+        let questions = Controller.myQuiz.quiz.map(questionAnswerSet => questionAnswerSet.question)
+        questions.forEach(question => {
+            Controller.myView.createQuestionBoxElement(question)
+        })
+
+        Controller.myQuiz.forAllAnswers(answer => {
+            Controller.myView.createAnswerCardElement(answer.answerText)
+        })
+
+        Controller.myView.shuffleContents('boxes')
+        Controller.myView.shuffleContents('answers')
+    }
+
+    static setUpEventListeners() {
+        window.addEventListener('submitEvent', Controller.submitEventHandler, false) // when "submit" button clicked
+        window.addEventListener('tryAgainEvent', Controller.tryAgainEventHandler, false) // when "Try Again" button clicked
+        window.addEventListener('scoreUpdateEvent', Controller.scoreUpdateEventHandler, false) // when quiz score is updated
+        window.addEventListener('resizeIframeEvent', Controller.resizeIframeEventHandler, false) // when an answer card is moved to a correct box
+        window.addEventListener('dropEvent', Controller.dropEventHandler, false) // when an answer card is dropped to a box
     }
 
     static submitEventHandler(event) {
-        let score = Math.round(Controller.theQuiz.score)
-        let passingScore = Controller.theQuiz.getPassingScore()
-        Controller.theQuiz.finish()
-        Controller.view.displayResult(score, passingScore)
-        Controller.view.sendScoreToMoodle(score)
+        let score = Controller.myQuiz.getRoundedQuizScore()
+        let passingScore = Controller.myQuiz.passingScore
+        Controller.myView.removeDraggableAll()
+        Controller.myView.displayResult(score, passingScore)
+        Controller.myView.sendScoreToMoodle(score)
     }
 
     static tryAgainEventHandler(event) {
@@ -27,11 +44,29 @@ class Controller {
     }
 
     static scoreUpdateEventHandler(event) {
-        let score = Math.round(Controller.theQuiz.score)
-        Controller.view.showCurrentScore(score)
+        let score = Controller.myQuiz.getRoundedQuizScore()
+        Controller.myView.updateCurrentScore(score)
     }
 
     static resizeIframeEventHandler(event) {
         parent.resizeIframe()
+    }
+
+    static dropEventHandler(event) {
+        let dropped = event.detail.question
+        let dragged = event.detail.answer
+
+        let target = Controller.myQuiz.quiz.find(questionAnswerSet => questionAnswerSet.question == dropped)
+        let foundAnswer = Controller.myQuiz.findAnswer(dragged)
+
+        if (target.answers.includes(foundAnswer)) {
+            // correct answer
+            Controller.myQuiz.addQuizScore(foundAnswer)
+            Controller.myView.removeDraggable(dragged)
+            Controller.myView.moveAnswerCardToBox(dragged, dropped)
+        } else {
+            // wrong answer
+            Controller.myQuiz.reduceAnswerScore(foundAnswer)
+        }
     }
 }
--- 20_Group/program/src/view.js
+++ 20_Group/program/src/view.js
@@ -2,8 +2,8 @@
 
 class View {
     static setUp() {
-        View.setUpSubmitBtn('btnSubmit')
-        View.setUpTryAgainBtn('btnTryAgain')
+        View.setUpSubmitBtn('btn-submit')
+        View.setUpTryAgainBtn('btn-try-again')
     }
 
     static fireCustomEvent(eventName) {
@@ -23,27 +23,103 @@ class View {
         }
     }
 
-    static getQuestionDiv() {
-        return document.getElementById('box')
+    // helper method to create question and answer element
+    static createDivElement(str) {
+        let div = document.createElement('div')
+        let p = document.createElement('p')
+        p.innerHTML = str
+        div.appendChild(p)
+        return div
     }
 
-    static getAnswerDiv() {
-        return document.getElementById('ans')
+    static createQuestionBoxElement(str) {
+        let boxContainer = document.getElementById('boxes')
+        let box = View.createDivElement(str)
+        box.classList.add('question-box')
+        boxContainer.appendChild(box)
+
+        $(box).droppable({
+            accept: '.answer-card',
+            hoverClass: 'hovered',
+            drop: (event, ui) => {
+                // use CustomEvent to pass data.
+                let eventInput = new CustomEvent('dropEvent', {
+                    'detail': {
+                        'question': str,
+                        'answer': ui.draggable[0].children[0].innerHTML
+                    }
+                })
+                window.dispatchEvent(eventInput)
+            }
+        })
     }
 
-    static getResultDiv() {
-        return document.getElementById('res')
+    static createAnswerCardElement(str) {
+        let answerContainer = document.getElementById('answers')
+        let answerCard = View.createDivElement(str)
+        answerCard.classList.add('answer-card')
+        answerContainer.appendChild(answerCard)
+
+        $(answerCard).draggable({
+            containment: 'body',
+            revert: true
+        })
     }
 
-    static showCurrentScore(score) {
-        let currentScoreElement = document.getElementById('currentScore')
+    // for shuffling question boxes and answer cards
+    static shuffleContents(tagId) {
+        let target = document.getElementById(tagId)
+        let divs = target.getElementsByTagName('div')
+        for (let i = 0; i < divs.length; i++) {
+            let randomDivNumber = Math.floor(Math.random() * divs.length)
+            target.appendChild(Array.from(divs).splice(randomDivNumber, 1)[0])
+        }
+    }
+
+    // find a question box or answer card of specific strings
+    static findDiv(tagId, str) {
+        let divs = document.getElementById(tagId).getElementsByTagName('div')
+        let foundDiv = Array.from(divs).find(div => {
+            let divContent = div.children[0].innerHTML
+            if (divContent == str) {
+                return div
+            }
+        })
+        return foundDiv
+    }
+
+    static moveAnswerCardToBox(answer, question) {
+        let answerDiv = View.findDiv('answers', answer)
+        let questionDiv = View.findDiv('boxes', question)
+        questionDiv.appendChild(answerDiv)
+        // resize iframe to fit height after possible expansion of question box
+        View.fireCustomEvent('resizeIframeEvent')
+    }
+
+    static removeDraggable(answer) {
+        let answerDiv = View.findDiv('answers', answer)
+        // setting revertDuration to 0 makes it look like snapping into a box
+        // revert is necessary because otherwise it keeps relative position
+        $(answerDiv).draggable('option', 'revertDuration', 0)
+        $(answerDiv).draggable('disable')
+    }
+
+    static removeDraggableAll() {
+        let divs = document.getElementById('answers').getElementsByTagName('div')
+        Array.from(divs).forEach(div => {
+            $(div).draggable('disable')
+        })
+    }
+
+    static updateCurrentScore(score) {
+        let currentScoreElement = document.getElementById('current-score')
         currentScoreElement.innerHTML = score
     }
 
     static sendScoreToMoodle(score) {
-        let FORM = window.parent.document.getElementById("store")
-        FORM.mark.value = score
-        //FORM.submit()
+        let form = window.parent.document.getElementById("store")
+        form.mark.value = score
+        //form.submit() //commented out because it refreshes page
     }
 
     static displayResult(score, passingScore) {
@@ -60,7 +136,7 @@ class View {
             finalMessage.innerHTML = 'Sorry you failed this time, but try again!'
         }
 
-        let resultElement = View.getResultDiv()
+        let resultElement = document.getElementById('result')
         resultElement.style.display = 'block'
     }
 }
