b58c2300537df9586b2af98ea2a2cea1b99fd49d
GervisChen <gervischen@gmail.com>
2017-06-28 05:16:15 -0400

    Delete main.js

--- src/main.js
+++
@@ -1,8 +0,0 @@
-//Call Javascript code automatically on page load. 
-//Instantiate Model, View and Controller.
-//Initialize Controller.
-$(document).ready(function(){
-    let controller = new QuizController(new Quiz);
-    let view = new QuizView(controller);
-    controller.initialize();
-});
